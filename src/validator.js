ecd79a319d9394cacc18f1a768aadd9efe2a4386
Goran Udosic <udosic.goran@gmail.com>
2017-07-26 15:59:13 +0200

    Lint

--- src/validator.js
+++ src/validator.js
@@ -1,39 +1,39 @@
 export const validate = (predicate, value, error) => {
-	if (typeof predicate !== 'function') {
-		throw new Error('Predicate should be a function');
-	}
+  if (typeof predicate !== 'function') {
+    throw new Error('Predicate should be a function');
+  }
 
-	if (typeof error !== 'string') {
-		throw new Error('Error should be a string');
-	}
+  if (typeof error !== 'string') {
+    throw new Error('Error should be a string');
+  }
 
-	const isValid = predicate(value);
+  const isValid = predicate(value);
 
-	if (isValid) {
-		return undefined;
-	}
+  if (isValid) {
+    return undefined;
+  }
 
-	return error;
+  return error;
 };
 
 export const validateMany = (validators = [], value) => {
-	const errors = validators.reduce((result, validator) => {
-		if (typeof validator.predicate !== 'function') {
-			throw new Error('validator.predicate should be a function, got ' + typeof validator.predicate);
-		}
+  const errors = validators.reduce((result, validator) => {
+    if (typeof validator.predicate !== 'function') {
+      throw new Error(`validator.predicate should be a function, got ${typeof validator.predicate}`);
+    }
 
-		if (typeof validator.error !== 'string') {
-			throw new Error('validator.error should be a string, got ' + typeof validator.error);
-		}
+    if (typeof validator.error !== 'string') {
+      throw new Error(`validator.error should be a string, got ${typeof validator.error}`);
+    }
 
-		const isValid = validator.predicate(value);
+    const isValid = validator.predicate(value);
 
-		if (!isValid) {
-			result.push(validator.error);
-		}
+    if (!isValid) {
+      result.push(validator.error);
+    }
 
-		return result;
-	}, []);
+    return result;
+  }, []);
 
-	return errors.length ? errors[0] : undefined;
+  return errors.length ? errors[0] : undefined;
 };
